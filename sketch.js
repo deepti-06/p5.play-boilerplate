615dc58f4005aa790707c80e3a674f1bfd8e0d7d
deepti-06 <52527169+deepti-06@users.noreply.github.com>
2020-09-22 16:43:52 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,9 +1,38 @@
+var fixedRect, movingRect;
+
 function setup() {
-  createCanvas(800,400);
-  createSprite(400, 200, 50, 50);
+  createCanvas(1200,800);
+
+fixedRect= createSprite(400,200,50,80)
+movingRect= createSprite(600,200,80,50)
+
+fixedRect.shapeColor= "green"
+movingRect.shapeColor= "green"
+
+
 }
 
 function draw() {
-  background(255,255,255);  
+  background(0,0,0);  
+
+  movingRect.x= mouseX;
+  movingRect.y= mouseY;
+
+
+if( movingRect.x- fixedRect.x < movingRect.width/2+fixedRect.width/2){
+
+  fixedRect.shapeColor= "red";
+  movingRect.shapeColor= "red";
+
+}
+else {
+  fixedRect.shapeColor= "green";
+  movingRect.shapeColor= "green";
+
+}
+
+
   drawSprites();
 }
+
+
